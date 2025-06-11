43bcdc7f438034b1a3b0c5debc0740518698d45c
monci089 <168063307+monci089@users.noreply.github.com>
2025-06-11 15:31:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,133 @@
 var playerlist=[]
 class targetlink{
-  constructor(target){
-    this.target=target
-    playerlist.append(this)
+  constructor(name){
+    this.target=null
+    this.name=name
+    playerlist.push(this)
   }
 }
-let Abby = new targetlink();
-let Aixa = new targetlink();
-let Alina = new targetlink();
-let Amber = new targetlink();
-let Arden = new targetlink();
-let Asher = new targetlink();
-let Bella = new targetlink();
-let Bjorn = new targetlink();
-let Chiara = new targetlink();
-let Cielo = new targetlink();
-let Clara = new targetlink();
-let Crenna = new targetlink();
-let Crosby = new targetlink();
-let Fern = new targetlink();
-let Frances = new targetlink();
-let Gavin = new targetlink();
-let Grace = new targetlink();
-let Gracie = new targetlink();
-let Halsey = new targetlink();
-let John = new targetlink();
-let Jonah = new targetlink();
-let Joshua = new targetlink();
-let Kaleb = new targetlink();
-let Kaydence = new targetlink();
-let Kepler = new targetlink();
-let Lenore = new targetlink();
-let Leny = new targetlink();
-let Lexi = new targetlink();
-let Linden = new targetlink();
-let Logan = new targetlink();
-let Luciano = new targetlink();
-let Luella = new targetlink();
-let Lynn = new targetlink();
-let Mdison = new targetlink();
-let Marie = new targetlink();
-let Mhalik = new targetlink();
-let Noah = new targetlink();
-let River = new targetlink();
-let SilasK = new targetlink();
-let SilasL = new targetlink();
-let Silja = new targetlink();
-let Soren = new targetlink();
-let Teri = new targetlink();
-let Tristyn = new targetlink();
-let Ursula = new targetlink();
-let Wylder = new targetlink();
-Abby.target = null;
-Aixa.target = null;
-Alina.target = null;
-Amber.target = null;
-Arden.target = null;
-Asher.target = null;
-Bella.target = null;
-Bjorn.target = null;
-Chiara.target = null;
-Cielo.target = null;
-Clara.target = null;
-Crenna.target = null;
-Crosby.target = null;
-Fern.target = null;
-Frances.target = null;
-Gavin.target = null;
-Grace.target = null;
-Gracie.target = null;
-Halsey.target = null;
-John.target = null;
-Jonah.target = null;
-Joshua.target = null;
-Kaleb.target = null;
-Kaydence.target = null;
-Kepler.target = null;
-Lenore.target = null;
-Leny.target = null;
-Lexi.target = null;
-Linden.target = null;
-Logan.target = null;
-Luciano.target = null;
-Luella.target = null;
-Lynn.target = null;
-Mdison.target = null;
+let Abby = new targetlink("Abby");
+let Aixa = new targetlink("Aixa");
+let Alina = new targetlink("Alina");
+let Amber = new targetlink("Amber");
+let Arden = new targetlink("Arden");
+let Asher = new targetlink("Asher");
+let Bella = new targetlink("Bella");
+let Bjorn = new targetlink("Bjorn");
+let Chiara = new targetlink("Chiara");
+let Cielo = new targetlink("Cielo");
+let Clara = new targetlink("Clara");
+let Crenna = new targetlink("Crenna");
+let Crosby = new targetlink("Crosby");
+let Fern = new targetlink("Fern");
+let Frances = new targetlink("Frances");
+let Gavin = new targetlink("Gavin");
+let Grace = new targetlink("Grace");
+let Gracie = new targetlink("Gracie");
+let Halsey = new targetlink("Halsey");
+let John = new targetlink("John");
+let Jonah = new targetlink("Jonah");
+let Joshua = new targetlink("Joshua");
+let Kaleb = new targetlink("Kaleb");
+let Kaydence = new targetlink("Kaydence");
+let Kepler = new targetlink("Kepler");
+let Lenore = new targetlink("Lenore");
+let Leny = new targetlink("Leny");
+let Lexi = new targetlink("Lexi");
+let Linden = new targetlink("Linden");
+let Logan = new targetlink("Logan");
+let Luciano = new targetlink("Luciano");
+let Luella = new targetlink("Luella");
+let Lynn = new targetlink("Lynn");
+let Mdison = new targetlink("Mdison");
+let Marie = new targetlink("Marie");
+let Mhalik = new targetlink("Mhalik");
+let Noah = new targetlink("Noah");
+let River = new targetlink("River");
+let SilasK = new targetlink("SilasK");
+let SilasL = new targetlink("SilasL");
+let Silja = new targetlink("Silja");
+let Soren = new targetlink("Soren");
+let Teri = new targetlink("Teri");
+let Tristyn = new targetlink("Tristyn");
+let Ursula = new targetlink("Ursula");
+let Wylder = new targetlink("Wylder");
+let Unknown= new targetlink("Unknown")
+Abby.target = Unknown;
+Aixa.target = Unknown;
+Alina.target = Unknown;
+Amber.target = Jonah;
+Arden.target = Tristyn;
+Asher.target = Unknown;
+Bella.target = Unknown;
+Bjorn.target = River;
+Chiara.target = Unknown;
+Cielo.target = Chiara;
+Clara.target = Unknown;
+Crenna.target = Unknown;
+Crosby.target = Unknown;
+Fern.target = Unknown;
+Frances.target = Unknown;
+Gavin.target = Grace;
+Grace.target = Unknown;
+Gracie.target = Unknown;
+Halsey.target = Unknown;
+John.target = Unknown;
+Jonah.target = Unknown;
+Joshua.target = Unknown;
+Kaleb.target = Unknown;
+Kaydence.target = Unknown;
+Kepler.target = Unknown;
+Lenore.target = Unknown;
+Leny.target = Unknown;
+Lexi.target = Unknown;
+Linden.target = Unknown;
+Logan.target = Unknown;
+Luciano.target = Unknown;
+Luella.target = Unknown;
+Lynn.target = Unknown;
+Mdison.target = Unknown;
 Marie.target = Gavin;
-Mhalik.target = null;
-Noah.target = null;
-River.target = null;
-SilasK.target = null;
-SilasL.target = null;
-Silja.target = null;
-Soren.target = null;
-Teri.target = null;
-Tristyn.target = null;
-Ursula.target = null;
-Wylder.target = null;
+Mhalik.target = Unknown;
+Noah.target = Unknown;
+River.target = Unknown;
+SilasK.target = Unknown;
+SilasL.target = Unknown;
+Silja.target = Unknown;
+Soren.target = Unknown;
+Teri.target = Unknown;
+Tristyn.target = Amber;
+Ursula.target = Unknown;
+Wylder.target = Unknown;
+
+function isnotin(name, list){
+  for (var t = 0; t < list.length; t++) {
+    if (list[t]==name){
+      return false;
+    }
+}
+return true;
+}
 function enter(){
-  const element = document.getElementById("text");
-  element.innerHTML = "list";
-  enter()
+  var targetlist=[]
+  var i=0;
+  var finished=[];
+  var pllength=playerlist.length;
+  while (i<pllength){
+    console.log(targetlist)
+      if (playerlist[i].target.name!="Unknown" && isnotin(playerlist[i].name, finished)){
+
+        targetlist.push(playerlist[i].name);
+        targetlist.push("-->");
+        targetlist.push(playerlist[i].target.name);
+        finished.push(playerlist[i].target.name);
+      }
+      if(!(isnotin(playerlist[i].name, finished))){
+        targetlist.splice(targetlist.indexOf(playerlist[i].name) + 1, 0, playerlist[i].target.name);
+        targetlist.splice(targetlist.indexOf(playerlist[i].name) + 1, 0, "-->");
+      }
+      i++;
+  }
+  console.log(targetlist);
 }
 enter()
